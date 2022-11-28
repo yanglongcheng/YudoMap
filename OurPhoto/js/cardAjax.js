f4759fa926bef2b80d484017ef0760c1499859ca
杨隆城 <103179143+yanglongcheng@users.noreply.github.com>
2022-11-28 13:46:46 +0800

    Update cardAjax.js

--- OurPhoto/js/cardAjax.js
+++ OurPhoto/js/cardAjax.js
@@ -1,11 +1,11 @@
 $(function() {
-	$('#hotCard').load('../../hotCard.html');
-	$('#nowCard').load('../../nowCard.html');
-	$('#headerBar').load('../../headerBar.html');
-	$('#footer').load('../../footer.html');
-	$('#sunPhoto').load('../../scenery/sunPhoto.html');
-	$('#seaPhoto').load('../../scenery/seaPhoto.html');
-	$('#forestPhoto').load('../../scenery/forestPhoto.html');
-	$('#PCPhoto').load('../../Games/PCGamesCard.html');
-	$('#phonePhoto').load('../../Games/phoneGamesCard.html');
+	$('#hotCard').load('./hotCard.html');
+	$('#nowCard').load('./nowCard.html');
+	$('#headerBar').load('./headerBar.html');
+	$('#footer').load('./footer.html');
+	$('#sunPhoto').load('./scenery/sunPhoto.html');
+	$('#seaPhoto').load('./scenery/seaPhoto.html');
+	$('#forestPhoto').load('./scenery/forestPhoto.html');
+	$('#PCPhoto').load('./Games/PCGamesCard.html');
+	$('#phonePhoto').load('./Games/phoneGamesCard.html');
 })
